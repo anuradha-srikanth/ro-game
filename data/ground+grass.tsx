7d6441046f63122a9695fb2af94191d1fe62a587
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-06-11 12:42:28 -0400

    small"

---
+++ data/ground+grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground+grass" tilewidth="10" tileheight="10" tilecount="400" columns="20">
+ <image source="../img/ground+grass.gif" width="200" height="200"/>
+</tileset>
