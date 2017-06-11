ff4961fbb835cd274261ba7eff7a3a1d1301ec70
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-06-11 12:39:19 -0400

    small"

---
+++ data/lion-front-up.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lion" tilewidth="150" tileheight="150" tilecount="4" columns="2">
+ <image source="../img/lion-front-up.png" width="300" height="300"/>
+</tileset>
---
+++ data/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="trees" tilewidth="1000" tileheight="500" tilecount="2" columns="1">
+ <image source="../img/background-trees.png" width="1000" height="1000"/>
+</tileset>
